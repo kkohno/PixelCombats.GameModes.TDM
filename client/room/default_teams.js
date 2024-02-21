f521d150a8849c9a84b1654b6d3a5db9eb355e0f
Konstantin <kkohno@mail.ru>
2024-02-21 20:32:17 +0700

    обновил либу по работе с командами

--- client/room/default_teams.js
+++ client/room/default_teams.js
@@ -4,15 +4,17 @@ export const RED_TEAM_NAME = "Red";
 export const BLUE_TEAM_NAME = "Blue";
 export const RED_TEAM_DISPLAY_NAME = "Teams/Red";
 export const BLUE_TEAM_DISPLAY_NAME = "Teams/Blue";
+export const BLUE_TEAM_SPAWN_POINTS_GROUP = 1;
+export const RED_TEAM_SPAWN_POINTS_GROUP = 2;
 
 export function create_team_red() {
     Teams.Add(RED_TEAM_NAME, RED_TEAM_DISPLAY_NAME, new Color(1, 0, 0, 0));
-    Teams.Get(RED_TEAM_NAME).Spawns.SpawnPointsGroups.Add(2);
+    Teams.Get(RED_TEAM_NAME).Spawns.SpawnPointsGroups.Add(RED_TEAM_SPAWN_POINTS_GROUP);
     return Teams.Get(RED_TEAM_NAME);
 }
 
 export function create_team_blue() {
     Teams.Add(BLUE_TEAM_NAME, BLUE_TEAM_DISPLAY_NAME, new Color(0, 0, 1, 0));
-    Teams.Get(BLUE_TEAM_NAME).Spawns.SpawnPointsGroups.Add(1);
+    Teams.Get(BLUE_TEAM_NAME).Spawns.SpawnPointsGroups.Add(BLUE_TEAM_SPAWN_POINTS_GROUP);
     return Teams.Get(BLUE_TEAM_NAME);
 }
