195ada20b6dc721495e3e1d45d75d1f89a7ff898
Konstantin <kkohno@mail.ru>
2024-03-02 04:29:26 +0700

    Update default_teams.js

--- client/room/default_teams.js
+++ client/room/default_teams.js
@@ -7,16 +7,18 @@ export const RED_TEAM_DISPLAY_NAME = "Teams/Red";
 export const BLUE_TEAM_DISPLAY_NAME = "Teams/Blue";
 export const BLUE_TEAM_SPAWN_POINTS_GROUP = 1;
 export const RED_TEAM_SPAWN_POINTS_GROUP = 2;
+export const BLUE_TEAM_COLOR = new Color(0, 0, 1, 0);
+export const RED_TEAM_COLOR = new Color(1, 0, 0, 0);
 
 export function create_team_blue() {
-    Teams.Add(BLUE_TEAM_NAME, BLUE_TEAM_DISPLAY_NAME, new Color(0, 0, 1, 0));
+    Teams.Add(BLUE_TEAM_NAME, BLUE_TEAM_DISPLAY_NAME, { b: 1 });
     const team = Teams.Get(BLUE_TEAM_NAME);
     team.Spawns.SpawnPointsGroups.Add(BLUE_TEAM_SPAWN_POINTS_GROUP);
     return team;
 }
 
 export function create_team_red() {
-    Teams.Add(RED_TEAM_NAME, RED_TEAM_DISPLAY_NAME, new Color(1, 0, 0, 0));
+    Teams.Add(RED_TEAM_NAME, RED_TEAM_DISPLAY_NAME, RED_TEAM_COLOR);
     const team = Teams.Get(RED_TEAM_NAME);
     team.Spawns.SpawnPointsGroups.Add(RED_TEAM_SPAWN_POINTS_GROUP);
     return team;
