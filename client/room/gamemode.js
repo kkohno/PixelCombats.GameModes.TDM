38cd3d1efc896816c87684b0cc0eee648c2eecf7
Konstantin <kkohno@mail.ru>
2025-02-11 12:58:16 +0700

    правки времени

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -8,9 +8,9 @@ const WaitingPlayersTime = 10;
 const BuildBaseTime = 30;
 const KnivesModeTime = 40;
 const GameModeTime = default_timer.game_mode_length_seconds();
-const MockModeTime = 20;
+const MockModeTime = 10;
 const EndOfMatchTime = 8;
-const VoteTime = 20;
+const VoteTime = 10;
 
 const KILL_SCORES = 5;
 const WINNER_SCORES = 10;
